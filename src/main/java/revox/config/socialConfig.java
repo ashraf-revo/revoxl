383b2444a483bf04a468e96a4c26030e72dccbbb
ashraf-revo <ashraf1abdelrasool@gmail.com>
2015-10-29 09:37:42 +0200

    Update socialConfig.java

--- src/main/java/revox/config/socialConfig.java
+++ src/main/java/revox/config/socialConfig.java
@@ -8,11 +8,13 @@ import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.web.ProviderSignInUtils;
 import org.springframework.social.security.AuthenticationNameUserIdSource;
+import org.springframework.social.config.annotation.EnableSocial;
 
 /**
  * Created by ashraf on 8/2/15.
  */
 @Configuration
+@EnableSocial
 public class socialConfig extends SocialConfigurerAdapter {
     @Override
     public UserIdSource getUserIdSource() {
